f4b2a8af35b005fc868d3c7f2a2f2ba93f446f91
Nathan M.S <natms24@gmail.com>
2024-12-20 20:07:54 +0300

    chore: add sample screenshots

--- server/controller/controller.go
+++ server/controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"fmt"
+	"log"
 	"paper-server/domain/dtos"
 	"paper-server/server/domain"
 )
@@ -17,6 +18,7 @@ func NewPaperStorage(usecase domain.IPaperStorageUsecase) *PaperStorage {
 }
 
 func (controller *PaperStorage) AddPaper(args dtos.AddPaperInput, reply *dtos.AddPaperOutput) error {
+	log.Printf("Called AddPaper with args: %+v", args)
 	paperNumber, err := controller.usecase.AddPaper(&args)
 	if err != nil {
 		return fmt.Errorf("%v", err)
@@ -27,6 +29,7 @@ func (controller *PaperStorage) AddPaper(args dtos.AddPaperInput, reply *dtos.Ad
 }
 
 func (controller *PaperStorage) ListPapers(args dtos.ListPaperInput, reply *dtos.ListPaperOuput) error {
+	log.Printf("Called ListPapers with args: %+v", args)
 	paperList, err := controller.usecase.ListPapers()
 	if err != nil {
 		return fmt.Errorf("%v", err)
@@ -37,6 +40,7 @@ func (controller *PaperStorage) ListPapers(args dtos.ListPaperInput, reply *dtos
 }
 
 func (controller *PaperStorage) GetPaperDetails(args dtos.GetPaperDetailsInput, reply *dtos.GetPaperDetailsOutput) error {
+	log.Printf("Called GetPaperDetails with args: %+v", args)
 	paperDetails, err := controller.usecase.GetPaperDetails(args.PaperNumber)
 	if err != nil {
 		return fmt.Errorf("%v", err)
@@ -50,6 +54,7 @@ func (controller *PaperStorage) GetPaperDetails(args dtos.GetPaperDetailsInput, 
 }
 
 func (controller *PaperStorage) FetchPaperContent(args dtos.FetchPaperContentInput, reply *dtos.FetchPaperContentOutput) error {
+	log.Printf("Called FetchPaperContent with args: %+v", args)
 	paper, err := controller.usecase.FetchPaperContent(args.PaperNumber)
 	if err != nil {
 		return fmt.Errorf("%v", err)
